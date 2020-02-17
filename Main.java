f3f24b6ac0da4186a874f26537d4035a2a5bd34e
Fishshhhh <2285746602@qq.com>
2020-02-18 02:52:43 +0800

    张三和王五两次对话

---
+++ Main.java
@@ -0,0 +1,10 @@
+package hello;
+
+public class Main {
+	public static void main(String[] args){
+		System.out.println("Hello,I'm ZhangSan");
+    	System.out.println("Hi,I'm WangWu");
+    	System.out.println("How are you?");
+    	System.out.println("I'm fine,thank you.");
+	}
+}
